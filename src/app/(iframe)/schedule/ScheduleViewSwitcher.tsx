1a5551046ee9e045414f4f9773af58d9a556cf32
qieman <253058016@qq.com>
2024-10-24 20:43:22 +0800

    Update ScheduleViewSwitcher.tsx

--- src/app/(iframe)/schedule/ScheduleViewSwitcher.tsx
+++ src/app/(iframe)/schedule/ScheduleViewSwitcher.tsx
@@ -10,11 +10,14 @@ export default function ScheduleViewSwitcher({weeklyUrl, dailyUrl, listingUrl, c
         boxShadow: '0px 1.988px 18px 0px rgba(0, 0, 0, 0.10)'
     }
     return <div className="flex-row-item-center rounded-[8px] bg-[#ececec] py-1 px-1 ml-4">
-        <a  style={currView === 'week' ? activeStyle:{}}
-            className="btn btn-xs btn-ghost text-sm w-[74px] rounded-[6px] hover:text-[#272928] text-[#C3C7C3] mr-1" href={weeklyUrl}>Week</a>
-        <a style={currView === 'day' ? activeStyle:{}}
-            className="btn btn-xs btn-ghost text-sm w-[74px] rounded-[6px] hover:text-[#272928] text-[#C3C7C3] mr-1" href={dailyUrl}>Day</a>
-        <a style={currView === 'list' ? activeStyle:{}}
-            className="btn btn-xs btn-ghost text-sm w-[74px] rounded-[6px] hover:text-[#272928] text-[#C3C7C3]" href={listingUrl}>List</a>
+        <a style={currView === 'list' ? activeStyle : {}}
+            className="btn btn-xs btn-ghost text-sm w-[74px] rounded-[6px] hover:text-[#272928] text-[#C3C7C3]"
+            href={listingUrl}>List</a>
+        <a style={currView === 'week' ? activeStyle : {}}
+            className="btn btn-xs btn-ghost text-sm w-[74px] rounded-[6px] hover:text-[#272928] text-[#C3C7C3] mr-1"
+            href={weeklyUrl}>Week</a>
+        <a style={currView === 'day' ? activeStyle : {}}
+            className="btn btn-xs btn-ghost text-sm w-[74px] rounded-[6px] hover:text-[#272928] text-[#C3C7C3] mr-1"
+            href={dailyUrl}>Day</a>
     </div>
 }
