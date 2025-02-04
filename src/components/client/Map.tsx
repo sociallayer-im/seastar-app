bd09085c10d153bfad1dc51122818b548a3fdb12
ZOU <253058016@qq.com>
2025-02-04 23:24:03 +0800

    Update Map.tsx

--- src/components/client/Map.tsx
+++ src/components/client/Map.tsx
@@ -4,24 +4,39 @@ import GoogleMapProvider from "@/providers/GoogleMapProvider"
 import {Map, AdvancedMarker, useMap} from '@vis.gl/react-google-maps'
 import {Dictionary} from '@/lang'
 import {Button} from '@/components/shadcn/Button'
-import {useState} from 'react'
+import {useMemo, useState} from 'react'
 
 export interface GoogleMapMarkerProps {
-    position: {lat: number, lng: number}
+    position: { lat: number, lng: number }
     title: string,
     pinIcon?: string
     onClick?: () => void
 }
 
 export interface GoogleMapProps {
-    center?: {lat: number, lng: number}
+    center?: { lat: number, lng: number }
     defaultZoom?: number
     markers: GoogleMapMarkerProps[]
     lang: Dictionary
 }
 
-export default function GoogleMap({center={lat: -34.397, lng: 150.644} ,markers, lang, defaultZoom=15}: GoogleMapProps) {
+export default function GoogleMap({
+                                      center = {lat: -34.397, lng: 150.644},
+                                      markers,
+                                      lang,
+                                      defaultZoom = 15
+                                  }: GoogleMapProps) {
     const [mapCenter, setMapCenter] = useState(center)
+
+    const markersToShow = useMemo(() => {
+        // set the layer of center marker to the top,
+        const centerMarker = markers.find(marker => marker.position.lat === mapCenter.lat && marker.position.lng === mapCenter.lng)
+        if (centerMarker) {
+            return [...markers.filter(marker => marker !== centerMarker), centerMarker]
+        } else {
+            return markers
+        }
+    }, [mapCenter])
 
     return <GoogleMapProvider>
         <Map mapId="e2f9ddc0facd5a80"
@@ -29,7 +44,7 @@ export default function GoogleMap({center={lat: -34.397, lng: 150.644} ,markers,
              defaultZoom={defaultZoom}
              disableDefaultUI>
             {
-                markers.map((marker, index) => {
+                markersToShow.map((marker, index) => {
                     const selected = mapCenter.lat === marker.position.lat && mapCenter.lng === marker.position.lng
                     return <MapMarker
                         onClick={() => setMapCenter(marker.position)}
@@ -42,7 +57,11 @@ export default function GoogleMap({center={lat: -34.397, lng: 150.644} ,markers,
     </GoogleMapProvider>
 }
 
-export function MapMarker({marker, selected, onClick}: {marker: GoogleMapMarkerProps, selected: boolean, onClick?: () => void}) {
+export function MapMarker({marker, selected, onClick}: {
+    marker: GoogleMapMarkerProps,
+    selected: boolean,
+    onClick?: () => void
+}) {
     const map = useMap()
     const handleClick = () => {
         if (map) {
@@ -58,13 +77,14 @@ export function MapMarker({marker, selected, onClick}: {marker: GoogleMapMarkerP
         position={marker.position}>
         <div className="flex flex-col items-center">
             <Button
-                style={selected? {background: 'linear-gradient(276deg,#f7df3a -18.27%,#d2f8e8 59.84%)'} : undefined}
+                style={selected ? {background: 'linear-gradient(276deg,#f7df3a -18.27%,#d2f8e8 59.84%)'} : undefined}
                 className="text-sm shadow !bg-background"
                 variant='secondary'
                 size="sm"
-            ><div className="overflow-ellipsis whitespace-nowrap overflow-hidden">
-                {marker.title}
-            </div>
+            >
+                <div className="overflow-ellipsis whitespace-nowrap overflow-hidden">
+                    {marker.title}
+                </div>
             </Button>
             <img className="w-[18px] h-auto" src={marker.pinIcon || '/images/map_marker.png'} alt=""/>
         </div>
