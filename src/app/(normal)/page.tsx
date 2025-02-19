8b27525d355820048c895cecc52538e9a34bde34
ZOU <253058016@qq.com>
2025-02-19 10:14:38 +0800

    Update page.tsx

--- src/app/(normal)/page.tsx
+++ src/app/(normal)/page.tsx
@@ -1,8 +1,16 @@
-import DiscoverPage from "@/app/(normal)/discover/page"
+import DiscoverPage, {generateMetadata as discoverGenerateMetadata} from "@/app/(normal)/discover/page"
 import GroupEventHome from '@/app/(normal)/event/[grouphandle]/GroupEventHome'
 import { headers } from "next/headers"
 import {GroupEventHomeDataProps} from '@/app/(normal)/event/[grouphandle]/data'
+import {generateMetadata as groupGenerateMetadata} from '@/app/(normal)/event/[grouphandle]/page'
 
+export async function generateMetadata(props: GroupEventHomeDataProps) {
+    const headersList = headers()
+    const eventHomeGroupHandle = headersList.get('x-event-home')
+    return !!eventHomeGroupHandle
+        ? await groupGenerateMetadata(props)
+        : await discoverGenerateMetadata()
+}
 
 export default async function Home(props: GroupEventHomeDataProps) {
     const headersList = headers()
