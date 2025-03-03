e1e74c719350bcfcd10530b67770173b95831a69
ZOU <253058016@qq.com>
2025-03-03 14:56:13 +0800

    Update HyperDx.tsx

--- src/components/client/HyperDx.tsx
+++ src/components/client/HyperDx.tsx
@@ -10,11 +10,11 @@ export default function HyperDx() {
     useEffect(() => {
         if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
             const apiURL = new URL(getSdkConfig(CLIENT_MODE).api).host
-            const regex = new RegExp(`${apiURL}`, "i");
+            const graphqlURL = new URL(getSdkConfig(CLIENT_MODE).graphql).host
             HyperDX.init({
                 apiKey: '5a926007-e14a-4a37-801b-40fcf982c6da',
                 service: 'my-frontend-app',
-                tracePropagationTargets: [regex], // Set to link traces from frontend to backend requests
+                tracePropagationTargets: [ new RegExp(`${apiURL}`, "i"), new RegExp(`${graphqlURL}`, "i")], // Set to link traces from frontend to backend requests
                 consoleCapture: true, // Capture console logs (default false)
                 advancedNetworkCapture: true, // Capture full HTTP request/response headers and bodies (default false)
             })
