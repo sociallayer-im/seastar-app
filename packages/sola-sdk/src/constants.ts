d8a4f8b141af2998481c80778b6d2493a1bfcba7
ZOU <253058016@qq.com>
2025-04-30 15:46:58 +0800

    Update constants.ts

--- packages/sola-sdk/src/constants.ts
+++ packages/sola-sdk/src/constants.ts
@@ -1,6 +1,6 @@
 export const PROD_NETWORK_CONFIG = {
     api: 'https://api.sola.day',
-    graphql: 'https://graph.sola.day/v1/graphql',
+    graphql: 'https://hasura-graph.fly.dev/v1/graphql',
 }
 
 export const DEV_NETWORK_CONFIG = {
