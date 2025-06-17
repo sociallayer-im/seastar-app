95567e2d5fab93637d2e4cbc8bfc77f81c703745
ZOU <253058016@qq.com>
2025-06-17 11:27:35 +0800

    Update ScheduleVenueView.tsx

--- src/app/(iframe)/schedule/venue/[grouphandle]/ScheduleVenueView.tsx
+++ src/app/(iframe)/schedule/venue/[grouphandle]/ScheduleVenueView.tsx
@@ -16,10 +16,10 @@ interface ScheduleVenueViewProps {
     authToken: string | null | undefined
 }
 
-export default function ScheduleCompactView({data: initialData, groupDetail, events: initialEvents, lang, authToken}: ScheduleVenueViewProps) {
+export default function ScheduleCompactView({ data: initialData, groupDetail, events: initialEvents, lang, authToken }: ScheduleVenueViewProps) {
     const [data, setData] = useState<IframeSchedulePageDataType>(initialData)
     const [events, setEvents] = useState<IframeSchedulePageDataEventDetail[]>(initialEvents)
-    
+
     // 生成时间标签，每15分钟一个
     const timeLabels = [] as string[]
     for (let hour = 0; hour < 24; hour++) {
@@ -39,26 +39,27 @@ export default function ScheduleCompactView({data: initialData, groupDetail, eve
     const timeStep = 15
 
     const [now, setNow] = useState<dayjs.Dayjs>(dayjs.tz(new Date(), groupDetail.timezone!))
-    const totalMinutes = now.diff(now.startOf('day'), 'minute');
+    const totalMinutes = now.diff(now.startOf('day'), 'minute')
+    const [showCursor, setShowCursor] = useState<boolean>(now.isSame(dayjs.tz(data.currDate, groupDetail.timezone!), 'date'))
 
 
     useEffect(() => {
-         // scroll cursor to view
-         const cursor = document.getElementById('curr-time-cursor')
-         if (cursor) {
-             cursor.scrollIntoView({ behavior: 'smooth', block: 'center' })
-         }
-        
-         const interval = setInterval(() => {
-            setNow(dayjs.tz(new Date(), groupDetail.timezone!))
-        }, 1000 * 60)
-        return () => clearInterval(interval)
-       
+        // scroll cursor to view
+        const cursor = document.getElementById('curr-time-cursor')
+        if (cursor) {
+            cursor.scrollIntoView({ behavior: 'smooth', block: 'center' })
+
+
+            const interval = setInterval(() => {
+                setNow(dayjs.tz(new Date(), groupDetail.timezone!))
+            }, 1000 * 60)
+            return () => clearInterval(interval)
+        }
     }, [])
 
-    
+
     return (
-        <div className="" style={{width: pageWidth}}>
+        <div className="" style={{ width: pageWidth }}>
             <div className="py-3 sm:py-5 w-full flex flex-row justify-between px-4 schedule-gradient">
                 <div className="sm:text-2xl text-xl">
                     <a href={data.eventHomeUrl} className="font-semibold text-[#6CD7B2] mr-2" target={data.isIframe ? "_blank" : "_self"}>
@@ -67,16 +68,20 @@ export default function ScheduleCompactView({data: initialData, groupDetail, eve
                     <span className="whitespace-nowrap">{lang['Event Schedule']}</span>
                 </div>
             </div>
-            
+
             <div className="grid sticky top-0 z-20" style={{
                 gridTemplateColumns: `${timeWidth}px repeat(${venues.length}, ${venueWidth}px)`,
                 gridTemplateRows: `${venueHeight}px`,
             }}>
                 <div className="border-r border-b border-t border-gray-200 bg-gray-50 sticky left-0 top-0 z-30" />
-                
+
                 {venues.map((venue, index) => (
-                    <div key={venue.id} className="border-r border-b border-t flex flex-col justify-center border-gray-200 bg-gray-50 p-2 text-center font-medium">
-                        {venue.title}
+                    <div key={venue.id}
+                        className="overflow-hidden text-sm border-r border-b border-t  border-gray-200 bg-gray-50 text-center font-medium relative">
+                        {venue.image_urls?.[0] && <img src={venue.image_urls?.[0]} alt="" className="w-full h-full object-cover" />}
+                        <div className="font-semibold p-3 absolute bottom-0 left-0 right-0 top-0 flex flex-col justify-end bg-gradient-to-b from-transparent via-[rgba(255,255,255,0.8)]  to-[rgba(255,255,255,1)] ">
+                            {venue.title}
+                        </div>
                     </div>
                 ))}
             </div>
@@ -93,8 +98,8 @@ export default function ScheduleCompactView({data: initialData, groupDetail, eve
                                 gridRow: timeIndex + 1,
                                 gridColumn: 1,
                             }}
-                            className="sticky left-0 z-10 border-r border-b border-gray-200 bg-gray-50 text-sm text-gray-500 flex items-center justify-center">
-                            {time}
+                            className="sticky left-0 z-10 border-r border-b-dashed border-gray-200 bg-gray-50 text-sm text-gray-500 flex items-center justify-center">
+                            {time !== '00:00' && <span className="text-xs -translate-y-[18px]">{time}</span>}
                         </div>
 
                         {venues.map((venue, venueIndex) => {
@@ -128,29 +133,32 @@ export default function ScheduleCompactView({data: initialData, groupDetail, eve
                         const height = duration / timeStep * timeHeight
 
                         return <VenueViewEventItem
-                        width={venueWidth}
-                        lang={lang}
-                        event={event}
-                        height={`${height}px`}
-                        top={`${y * timeHeight}px`}
-                        left={`${x * venueWidth - (venueWidth - timeWidth)}px`}
-                    />
+                            width={venueWidth}
+                            lang={lang}
+                            event={event}
+                            height={`${height}px`}
+                            top={`${y * timeHeight}px`}
+                            left={`${x * venueWidth - (venueWidth - timeWidth)}px`}
+                        />
                     })
                 }
 
-                <div id="curr-time-cursor" className='w-full absolute border-t border-red-500 text-white'
-                    style={{
-                        zIndex: 10,
-                        top: `${totalMinutes / timeStep * timeHeight}px`,
-                        left: 0,
-                        height: '1px',
-                    }}
-                >
+                {
+                    showCursor && (
+                        <div id="curr-time-cursor" className='w-full absolute border-t border-red-500 text-white'
+                            style={{
+                                zIndex: 10,
+                                top: `${totalMinutes / timeStep * timeHeight}px`,
+                                left: 0,
+                                height: '1px',
+                            }}>
 
-                    <span className="text-xs bg-red-500  px-3 rounded-full translate-y-[-82%] inline-flex translate-x-1 font-semibold">
-                        {now.format('HH:mm')}
-                        </span>
-                </div>
+                            <span className="text-xs bg-red-500  px-3 rounded-full translate-y-[-82%] inline-flex translate-x-1 font-semibold">
+                                {now.format('HH:mm')}
+                            </span>
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
